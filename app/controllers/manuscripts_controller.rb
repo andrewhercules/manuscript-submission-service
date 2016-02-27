065aabd055c3c0eaa3c0a699e357f74cc4e4d4f3
Andrew Hercules <andrewhercules@gmail.com>
2016-02-27 15:24:44 +0000

    Added test for when user submits a new manuscript

--- app/controllers/manuscripts_controller.rb
+++ app/controllers/manuscripts_controller.rb
@@ -4,4 +4,20 @@ class ManuscriptsController < ApplicationController
     @manuscripts = Manuscript.all
   end
 
+  def new
+    @manuscript = Manuscript.new
+  end
+
+  def create
+    Manuscript.create(manuscript_params)
+    redirect_to '/manuscripts'
+  end
+
+
+# Utility method to explicitly state params accepted by controller
+
+  def manuscript_params
+    params.require(:manuscript).permit(:title, :author, :journal)
+  end
+
 end
--- spec/features/manuscripts_feature_spec.rb
+++ spec/features/manuscripts_feature_spec.rb
@@ -26,4 +26,19 @@ feature 'manuscripts' do
 
   end
 
+  context 'creating a manuscript' do
+
+    scenario 'user is prompted to fill out a form, then browser displays the new manuscript' do
+      visit '/manuscripts'
+      click_link 'Add a manuscript'
+      fill_in 'Title', with: 'My new manuscript for submission'
+      fill_in 'Author', with: 'Lead Author Name'
+      fill_in 'Journal', with: 'Journal of Academic Studies'
+      click_button 'Submit Manuscript'
+      expect(page).to have_content 'My new manuscript for submission'
+      expect(current_path).to eq '/manuscripts'
+    end
+
+  end
+
 end
